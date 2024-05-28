bd0270fa38d80fd66ebc3747929cabcf45882143
yasagy222 <yasaghy222@gmail.com>
2024-05-28 18:41:21 +0330

    fix add edit

--- Controllers/ClinicController.cs
+++ Controllers/ClinicController.cs
@@ -18,14 +18,14 @@ public class ClinicController(DoctorServiceContext context,
 	[HttpPut]
 	public async Task<IActionResult> Put(ClinicDto model)
 	{
-		Result result = await _service.Add(model);
+		Result result = await _service.Edit(model);
 		return StatusCode(result.StatusCode, result.Data);
 	}
 
 	[HttpPost]
 	public async Task<IActionResult> Post(ClinicDto model)
 	{
-		Result result = await _service.Edit(model);
+		Result result = await _service.Add(model);
 		return StatusCode(result.StatusCode, result.Data);
 	}
 }
